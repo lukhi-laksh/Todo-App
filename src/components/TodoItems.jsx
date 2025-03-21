c0fc8e880f1f9e8d4a2d33a3fd265559fd261b04
laksh_lukhi <140299225+lukhi-laksh@users.noreply.github.com>
2025-03-21 23:29:49 +0530

    Update TodoItems.jsx
    do it same as before

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -5,11 +5,6 @@ const TodoItems = () => {
                 { id: 2, todoText: "Go To Collage", todoDate: "Week Day" },
                 { id: 3, todoText: "Exsersize", todoDate: "Everyday" },
               ];
-                const todoItem1 = [
-                { id: 1, todoText: "Buy Milk", todoDate: "22-Dec-2004" },
-                { id: 2, todoText: "Go To Collage", todoDate: "Week Day" },
-                { id: 3, todoText: "Exsersize", todoDate: "Everyday" },
-              ];
         return todoItem.map(item => <TodoItem key={item.id} id={item.id} todoText={item.todoText} todoDate={item.todoDate} />)
 }
 
